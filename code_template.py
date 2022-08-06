23cb2f101de0056f41ca37455105a8011fc07858
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-08-06 20:39:18 +0200

    Some serious refactor tractor and improvement of the code template.

--- code_template.py
+++ code_template.py
@@ -70,7 +70,7 @@ def calc_score(state):
     """
     Reward function for your problem.
     Input: a 0-1 vector of length DECISIONS. It represents the graph (or other object) you have created.
-    Output: the reward/score for your construction. See files in the *demos* folder for examples.	
+    Output: the reward/score for your construction. See files in the *demos* folder for examples.    
     """
     G = nx.Graph()
     G.add_nodes_from(list(range(N)))
@@ -89,119 +89,105 @@ def calc_score(state):
     return nxa.diameter(G) / G.size()
 
 
-####No need to change anything below here. 
-def play_game(n_sessions, actions,state_next,states,prob, step, total_score):
-	for i in range(n_sessions):
-		if np.random.rand() < prob[i]:
-			action = 1
-		else:
-			action = 0
-		actions[i][step-1] = action
-		state_next[i] = states[i,:,step-1]
-
-		if (action > 0):
-			state_next[i][step-1] = action
-		state_next[i][DECISIONS + step-1] = 0
-		if (step < DECISIONS):
-			state_next[i][DECISIONS + step] = 1
-
-		#calculate final score
-		terminal = step == DECISIONS
-		if terminal:
-			total_score[i] = calc_score(state_next[i])
-
-		# record sessions 
-		if not terminal:
-			states[i,:,step] = state_next[i]
-
-	return actions, state_next,states, total_score, terminal
+#### No need to change anything below here. 
 
 
-def generate_session(agent, n_sessions, verbose = 1):
-	"""
-	Play n_session games using agent neural network.
-	"""
-	states =  np.zeros([n_sessions, observation_space, len_game], dtype=int)
-	actions = np.zeros([n_sessions, len_game], dtype = int)
-	state_next = np.zeros([n_sessions,observation_space], dtype = int)
-	prob = np.zeros(n_sessions)
-	states[:,DECISIONS,0] = 1
-	step = 0
-	total_score = np.zeros([n_sessions])
-	pred_time = 0
-	play_time = 0
+def take_action(n, actions, states, games, prob, step, total_score):
+    """
+    Samples a single action for each of the `n` games using the given action 
+    `prob`abilities, and records the result in the actions/games tensors.
 
-	while (True):
-		step += 1
-		tic = time.time()
-		prob = agent.predict(states[:,:,step-1], batch_size = n_sessions) 
-		pred_time += time.time()-tic
-		tic = time.time()
-		actions, state_next,states, total_score, terminal = play_game(n_sessions, actions,state_next,states,prob, step, total_score)
-		play_time += time.time()-tic
-		if terminal:
-			break
+    Inputs:
+        n: number of games to sample actions for
+        actions: a tensor of size `n` x `len_game`
+        states: a tensor of size `n` x `observation_space`
+        games: a tensor of size `n` x `observation_space` x `len_game`
+        prob: a vector of size `n` of action probabilities
+        total_score: a vector of size `n` of rewards for each game
+    """
+    # So we could see huge performance benefits by batching inference here.
+    for i in range(n):
+        if np.random.rand() < prob[i]:
+            action = 1
+        else:
+            action = 0
+        actions[i][step-1] = action
+        states[i] = games[i,:,step-1]
 
-	if (verbose):
-		print("Predict: "+str(pred_time)+", play: " + str(play_time))
+        if (action > 0):
+            states[i][step-1] = action
+        states[i][DECISIONS + step-1] = 0
+        if (step < DECISIONS):
+            states[i][DECISIONS + step] = 1
 
-	return states, actions, total_score
+        # calculate final score
+        terminal = step == DECISIONS
+        if terminal:
+            total_score[i] = calc_score(states[i])
 
+        # record sessions 
+        if not terminal:
+            games[i,:,step] = states[i]
 
-def select_elites(states_batch, actions_batch, rewards_batch, percentile=50):
-	"""
-	Select states and actions from games that have rewards >= percentile
-	:param states_batch: list of lists of states, states_batch[session_i][t]
-	:param actions_batch: list of lists of actions, actions_batch[session_i][t]
-	:param rewards_batch: list of rewards, rewards_batch[session_i]
-
-	:returns: elite_states,elite_actions, both 1D lists of states and respective actions from elite sessions
-
-	This function was mostly taken from https://github.com/yandexdataschool/Practical_RL/blob/master/week01_intro/deep_crossentropy_method.ipynb
-	If this function is the bottleneck, it can easily be sped up using numba
-	"""
-	counter = n_sessions * (100.0 - percentile) / 100.0
-	reward_threshold = np.percentile(rewards_batch,percentile)
-
-	elite_states = []
-	elite_actions = []
-	elite_rewards = []
-	for i in range(len(states_batch)):
-		if rewards_batch[i] >= reward_threshold-0.0000001:
-			if (counter > 0) or (rewards_batch[i] >= reward_threshold+0.0000001):
-				for item in states_batch[i]:
-					elite_states.append(item.tolist())
-				for item in actions_batch[i]:
-					elite_actions.append(item)
-			counter -= 1
-	elite_states = np.array(elite_states, dtype = int)
-	elite_actions = np.array(elite_actions, dtype = int)
-	return elite_states, elite_actions
+    return actions, states,games, total_score, terminal
 
 
-def select_super_sessions(states_batch, actions_batch, rewards_batch, percentile=90):
-	"""
-	Select all the sessions that will survive to the next generation
-	Similar to select_elites function
-	If this function is the bottleneck, it can easily be sped up using numba
-	"""
-	counter = n_sessions * (100.0 - percentile) / 100.0
-	reward_threshold = np.percentile(rewards_batch,percentile)
+def sample(agent, n):
+    """
+    Samples `n` games from the probabilistic policy defined by the `agent`'s
+    neural network.
 
-	super_states = []
-	super_actions = []
-	super_rewards = []
-	for i in range(len(states_batch)):
-		if rewards_batch[i] >= reward_threshold-0.0000001:
-			if (counter > 0) or (rewards_batch[i] >= reward_threshold+0.0000001):
-				super_states.append(states_batch[i])
-				super_actions.append(actions_batch[i])
-				super_rewards.append(rewards_batch[i])
-				counter -= 1
-	super_states = np.array(super_states, dtype = int)
-	super_actions = np.array(super_actions, dtype = int)
-	super_rewards = np.array(super_rewards)
-	return super_states, super_actions, super_rewards
+    Inputs:
+        agent: a neural network defining the agent's policy
+        n: the number of games to sample
+    Outputs:
+        games: a tensor of size `n` x `observation_space` x `len_game`
+        actions: a tensor of size `n` x `len_game`
+        total_score: a vector of size `n`
+    """
+    games =  np.zeros([n, observation_space, len_game], dtype=int)
+    actions = np.zeros([n, len_game], dtype = int)
+    total_score = np.zeros([n])
+    games[:,DECISIONS,0] = 1 # sets initial 1-hot vector for each game
+
+    # Used as a temporary register during game computation.
+    states = np.zeros([n, observation_space], dtype = int)
+
+    step = 0
+    while (True):
+        step += 1
+        prob = agent.predict(games[:,:,step-1], batch_size = n_sessions) 
+        actions, states,games, total_score, is_terminal = take_action(n_sessions, actions,states,games,prob, step, total_score)
+        if is_terminal:
+            break
+
+    return games, actions, total_score
+
+
+def top_percentile(states_batch, actions_batch, rewards_batch, percentile=90):
+    """
+    This function returns the top `percentile`% of the games and actions in
+    the given batch with respect to their rewards.
+    """
+    counter = n_sessions * (100.0 - percentile) / 100.0
+    reward_threshold = np.percentile(rewards_batch, percentile)
+
+    top_states = []
+    top_actions = []
+    top_rewards = []
+    for i in range(len(states_batch)):
+        if rewards_batch[i] >= reward_threshold-0.0000001:
+            if (counter > 0) or (rewards_batch[i] >= reward_threshold+0.0000001):
+                top_states.append(states_batch[i])
+                top_actions.append(actions_batch[i])
+                top_rewards.append(rewards_batch[i])
+                counter -= 1
+    top_states = np.array(top_states, dtype = int)
+    top_actions = np.array(top_actions, dtype = int)
+    top_rewards = np.array(top_rewards)
+    return top_states, top_actions, top_rewards
+
+
 
 
 super_states =  np.empty((0,len_game,observation_space), dtype = int)
@@ -210,82 +196,60 @@ super_rewards = np.array([])
 sessgen_time = 0
 fit_time = 0
 score_time = 0
-
-
-
 myRand = random.randint(0,1000) #used in the filename
 
 for i in range(1000000): #1000000 generations should be plenty
-	#generate new sessions
-	#performance can be improved with joblib
-	tic = time.time()
-	sessions = generate_session(model,n_sessions,0) #change 0 to 1 to print out how much time each step in generate_session takes 
-	sessgen_time = time.time()-tic
-	tic = time.time()
+    games, actions, total_score = sample(model, n_sessions)
 
-	states_batch = np.array(sessions[0], dtype = int)
-	actions_batch = np.array(sessions[1], dtype = int)
-	rewards_batch = np.array(sessions[2])
-	states_batch = np.transpose(states_batch,axes=[0,2,1])
+    states_batch = np.array(games, dtype = int)
+    actions_batch = np.array(actions, dtype = int)
+    rewards_batch = np.array(total_score)
+    states_batch = np.transpose(states_batch,axes=[0,2,1])
 
-	states_batch = np.append(states_batch,super_states,axis=0)
+    # NOTE(guy): The use of super_ stuff is to keep the best performing samples
+    # across all generations, so we can keep learning from them! Although that
+    # kinda seems to me like we should be doing more training cycles per batch.
+    states_batch = np.append(states_batch, super_states, axis=0)
+    if i>0:
+        actions_batch = np.append(actions_batch, np.array(super_actions), axis=0)
+    rewards_batch = np.append(rewards_batch, super_rewards)
 
-	if i>0:
-		actions_batch = np.append(actions_batch,np.array(super_actions),axis=0)	
-	rewards_batch = np.append(rewards_batch,super_rewards)
+    elite_states, elite_actions, _ = top_percentile(states_batch, actions_batch, rewards_batch, percentile=percentile) #pick the sessions to learn from
+    elite_states = elite_states.reshape((elite_states.shape[0]*elite_states.shape[1], elite_states.shape[2])) # flatten first two dims
+    elite_actions = elite_actions.reshape((elite_actions.shape[0]*elite_actions.shape[1]))
+    model.fit(elite_states, elite_actions) # TODO: run this multiple times?
 
-	randomcomp_time = time.time()-tic 
-	tic = time.time()
+    super_sessions = top_percentile(states_batch, actions_batch, rewards_batch, percentile=super_percentile) #pick the sessions to survive
+    super_sessions = [(super_sessions[0][i], super_sessions[1][i], super_sessions[2][i]) for i in range(len(super_sessions[2]))]
+    super_sessions.sort(key=lambda super_sessions: super_sessions[2],reverse=True)
 
-	elite_states, elite_actions = select_elites(states_batch, actions_batch, rewards_batch, percentile=percentile) #pick the sessions to learn from
-	select1_time = time.time()-tic
+    super_states = [super_sessions[i][0] for i in range(len(super_sessions))]
+    super_actions = [super_sessions[i][1] for i in range(len(super_sessions))]
+    super_rewards = [super_sessions[i][2] for i in range(len(super_sessions))]
 
-	tic = time.time()
-	super_sessions = select_super_sessions(states_batch, actions_batch, rewards_batch, percentile=super_percentile) #pick the sessions to survive
-	select2_time = time.time()-tic
+    rewards_batch.sort()
+    mean_all_reward = np.mean(rewards_batch[-100:])
+    mean_best_reward = np.mean(super_rewards)
 
-	tic = time.time()
-	super_sessions = [(super_sessions[0][i], super_sessions[1][i], super_sessions[2][i]) for i in range(len(super_sessions[2]))]
-	super_sessions.sort(key=lambda super_sessions: super_sessions[2],reverse=True)
-	select3_time = time.time()-tic
+    print(f"\n{i}. Best individuals: {np.flip(np.sort(super_rewards))}")
+    print(f"Mean reward: {mean_all_reward}")
 
-	tic = time.time()
-	model.fit(elite_states, elite_actions) #learn from the elite sessions
-	fit_time = time.time()-tic
-
-	tic = time.time()
-
-	super_states = [super_sessions[i][0] for i in range(len(super_sessions))]
-	super_actions = [super_sessions[i][1] for i in range(len(super_sessions))]
-	super_rewards = [super_sessions[i][2] for i in range(len(super_sessions))]
-
-	rewards_batch.sort()
-	mean_all_reward = np.mean(rewards_batch[-100:])	
-	mean_best_reward = np.mean(super_rewards)	
-
-	score_time = time.time()-tic
-
-	print("\n" + str(i) +  ". Best individuals: " + str(np.flip(np.sort(super_rewards))))
-
-	#uncomment below line to print out how much time each step in this loop takes. 
-	print(	"Mean reward: " + str(mean_all_reward) + "\nSessgen: " + str(sessgen_time) + ", other: " + str(randomcomp_time) + ", select1: " + str(select1_time) + ", select2: " + str(select2_time) + ", select3: " + str(select3_time) +  ", fit: " + str(fit_time) + ", score: " + str(score_time))
-
-	if (i%20 == 1): #Write all important info to files every 20 iterations
-		with open('best_species_pickle_'+str(myRand)+'.txt', 'wb') as fp:
-			pickle.dump(super_actions, fp)
-		with open('best_species_txt_'+str(myRand)+'.txt', 'w') as f:
-			for item in super_actions:
-				f.write(str(item))
-				f.write("\n")
-		with open('best_species_rewards_'+str(myRand)+'.txt', 'w') as f:
-			for item in super_rewards:
-				f.write(str(item))
-				f.write("\n")
-		with open('best_100_rewards_'+str(myRand)+'.txt', 'a') as f:
-			f.write(str(mean_all_reward)+"\n")
-		with open('best_elite_rewards_'+str(myRand)+'.txt', 'a') as f:
-			f.write(str(mean_best_reward)+"\n")
-	if (i%200==2): # To create a timeline, like in Figure 3
-		with open('best_species_timeline_txt_'+str(myRand)+'.txt', 'a') as f:
-			f.write(str(super_actions[0]))
-			f.write("\n")
+    if (i%20 == 1): #Write all important info to files every 20 iterations
+        with open('best_species_pickle_'+str(myRand)+'.txt', 'wb') as fp:
+            pickle.dump(super_actions, fp)
+        with open('best_species_txt_'+str(myRand)+'.txt', 'w') as f:
+            for item in super_actions:
+                f.write(str(item))
+                f.write("\n")
+        with open('best_species_rewards_'+str(myRand)+'.txt', 'w') as f:
+            for item in super_rewards:
+                f.write(str(item))
+                f.write("\n")
+        with open('best_100_rewards_'+str(myRand)+'.txt', 'a') as f:
+            f.write(str(mean_all_reward)+"\n")
+        with open('best_elite_rewards_'+str(myRand)+'.txt', 'a') as f:
+            f.write(str(mean_best_reward)+"\n")
+    if (i%200==2): # To create a timeline, like in Figure 3
+        with open('best_species_timeline_txt_'+str(myRand)+'.txt', 'a') as f:
+            f.write(str(super_actions[0]))
+            f.write("\n")
